0c2753b3193ad4d01a6cd5d80d2c07da1737931a
zoejohnson <zmn.johnson@gmail.com>
2015-08-22 21:44:39 -0700

    Used comments to help me walk through the http-client problem. After I passed, I added their 'official solution' to compare.

--- learnyounode/http-client.js
+++ learnyounode/http-client.js
@@ -3,11 +3,27 @@
 // the String contents of each "data" event from the response
 // to a new line on the console.
 
-var http = require('http')
-var url = process.argv[2]
+var http = require('http'), url = process.argv[2]
 
-http.get(url, callback)
+http.get(url, function callback (response) { // response is a Stream object
+  // need to convert data to String
+  response.setEncoding('utf8')
+  // 'Listen' to the data event
+  response.on('data', function (data) {
 
-function callback (response) { // response is a Stream object
-  
-}
+    // need to print data contents on new lines to console
+    console.log(data)
+  })
+});
+
+
+// Their official solution:
+// var http = require('http')
+//
+//    http.get(process.argv[2], function (response) {
+//      response.setEncoding('utf8')
+//      response.on('data', console.log)
+//      response.on('error', console.error)
+//    })
+
+// How do I know to add an error?
